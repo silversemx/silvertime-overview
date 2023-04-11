ce6aa6b6c3df2b849206c9335af0e8e05e1ff539
Luis Huerta <luisgerhuerta@gmail.com>
2023-04-11 09:53:54 -0600

    Added get_all_maintenances action function in Maintenance component

--- src/components/maintenance/Maintenance.js
+++ src/components/maintenance/Maintenance.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 // React Bootstrap
@@ -8,11 +9,11 @@ import { Card, Container } from 'react-bootstrap';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-// Utils
-import maintenanceJSON from '../../utils/maintenance.json'
+// Actions
+import { get_all_maintenances } from '../../redux/actions/maintenancesActions';
 
 const Post = (props) => {
-	const { id, title, schedule, msg, postedOn } = props;
+	const { maintenance } = props;
 
 	const navigateTo = useNavigate();
 
@@ -26,33 +27,37 @@ const Post = (props) => {
 				>
 					{title}
 				</p>
-				<p className='schedule-text text-muted m-0'>From: {new Date(schedule?.from).toString('es-MX', { timeZone: 'CST' })}</p>
-				<p className='schedule-text text-muted'>To: {new Date(schedule?.to).toString('es-MX', { timeZone: 'CST' })}</p>
+				<p className='schedule-text text-muted m-0'>From: {new Date(maintenance?.start?.$date).toString('es-MX', { timeZone: 'CST' })}</p>
+				<p className='schedule-text text-muted'>To: {new Date(maintenance?.end?.$date).toString('es-MX', { timeZone: 'CST' })}</p>
 				<hr/>
 				<ReactQuill
 					theme='bubble'
 					readOnly={true}
-					value={msg}
+					value={JSON.parse(maintenance?.text)}
 				/>
-				<p className='schedule-text text-muted text-end mt-5'>Posted on: {new Date(postedOn).toString('es-MX', { timeZone: 'CST' })}</p>
+				<p className='schedule-text text-muted text-end'>Posted on: {new Date(maintenance?.date?.$date).toString('es-MX', { timeZone: 'CST' })}</p>
 			</Card.Body>
 		</Card>
 	);
 }
 
 const Maintenance = () => {
+	const dispatch = useDispatch();
+
+	const { all_maintenances } = useSelector(state => state.maintenances);
+
+	useEffect(() => {
+		dispatch(get_all_maintenances());
+	}, []);
+
 	return (
 		<Container className='custom-container'>
 			<h2>Scheduled Maintenance</h2>
 			<p className='mb-4'>This page is used to list the scheduled maintenance to be performed on the services.</p>
-			{maintenanceJSON.maintenance.map((post, idx) => (
+			{all_maintenances.maintenances.map((maintenance, idx) => (
 				<Post
 					key={idx}
-					id={post?.id}
-					title={post?.title}
-					schedule={post?.schedule}
-					msg={post?.msg}
-					postedOn={post?.postedOn}
+					maintenance={maintenance}
 				/>
 			))}
 
