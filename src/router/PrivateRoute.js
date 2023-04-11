6e7752ca2ada3de971300f748a54d56739ef01a3
Luis Huerta <luisgerhuerta@gmail.com>
2023-04-10 21:29:03 -0600

    Added base PrivateRoute component

---
+++ src/router/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const PrivateRoute = () => {
+	const isAuthenticated = sessionStorage.getItem('isAuthenticated');
+
+	// If authorized, return an outlet that will render child elements
+	// If not, return element that will navigate to login page
+	
+	return isAuthenticated ? <Outlet /> : <Navigate to='/auth' />;
+}
+
+export default PrivateRoute;
