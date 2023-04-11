ce176c785bd23b709717a716ccd43851314d1af8
Luis Huerta <luisgerhuerta@gmail.com>
2023-04-10 21:36:31 -0600

    Protected routes with PrivateRoute component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 // Components
 import Header from './components/main/Header';
 import Footer from './components/main/Footer';
+import Authentication from './components/main/Authentication';
+import PrivateRoute from './router/PrivateRoute';
 import Services from './components/services/Services';
 import Maintenance from './components/maintenance/Maintenance';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+
+// Actions
+import { user_token_check } from './redux/actions/authActions';
+user_token_check (store);
 
 const App = () => {
 	return (
@@ -20,11 +26,15 @@ const App = () => {
 				<div className='App'>
 					<Header/>
 					<Routes>
-						<Route path='/' element={ <Navigate to='/services' /> } />
-						
-						<Route path='/services' element={ <Services /> } />
-						
-						<Route path='/maintenance' element={ <Maintenance /> } />
+						<Route path='/auth' element={ <Authentication /> } />
+
+						<Route element={ <PrivateRoute />}>
+							<Route path='/' element={ <Navigate to='/services' /> } />
+							
+							<Route path='/services' element={ <Services /> } />
+							
+							<Route path='/maintenance' element={ <Maintenance /> } />
+						</Route>
 					</Routes>
 					<Footer/>
 				</div>
