1feb28b624f251d7ed9704b9f807df055ee77682
Luis Huerta <luisgerhuerta@gmail.com>
2023-04-12 12:05:02 -0600

    Added util function to get execution scope description

--- src/utils/getStatusDesc.js
+++ src/utils/getStatusDesc.js
@@ -18,6 +18,27 @@ const getMaintenanceStatusDesc = (status) => {
 	return desc;
 }
 
+// EXECUTION_SCOPE_NONE = 0
+// EXECUTION_SCOPE_GLOBAL = 1
+// EXECUTION_SCOPE_SERVICE = 2
+// EXECUTION_SCOPE_INSTANCE = 3
+// EXECUTION_SCOPE_OTHER = 4
+const getExecutionScopeDesc = (status) => {
+	let desc = '';
+
+	switch (parseInt(status)) {
+		case 0:		desc = 'None';					break;
+		case 1:		desc = 'Global';				break;
+		case 2:		desc = 'Service';				break;
+		case 3:		desc = 'Instance';			break;
+		case 4:		desc = 'Other';					break;
+		default:	desc = 'None';					break;
+	}
+
+	return desc;
+}
+
 export { 
-	getMaintenanceStatusDesc
+	getMaintenanceStatusDesc,
+	getExecutionScopeDesc,
 };
