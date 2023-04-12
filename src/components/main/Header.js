c40d1864d564bbe64784118fc2aacc0f5b9ad09f
Luis Huerta <luisgerhuerta@gmail.com>
2023-04-12 15:46:51 -0600

    Updated condition to highlight service nav link

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
 				<Navbar.Collapse id='navbarScroll'>
 					<Nav className='w-100 me-auto' navbarScroll>
 						<Container className='nav-container' fluid>
-							<Nav.Link href='/services' className='header-link' active={location.pathname.includes('/services')}>Services</Nav.Link>
+							<Nav.Link href='/services' className='header-link' active={location.pathname.includes('/service')}>Services</Nav.Link>
 							<Nav.Link href='/maintenance' className='header-link' active={location.pathname.includes('/maintenance')}>Maintenance</Nav.Link>
 							<Nav.Link href='/incidents' className='header-link' active={location.pathname.includes('/incidents')}>Incidents</Nav.Link>
 							<Nav.Link href='/reports' className='header-link' active={location.pathname.includes('/reports')}>Reports</Nav.Link>
