91d7a2c607d3b0532fead8e4e6bb80594f2e12fc
Luis Huerta <luisgerhuerta@gmail.com>
2023-04-12 15:36:48 -0600

    Added interruption solution in InterruptionInfo

--- src/components/interruptions/InterruptionInfo.js
+++ src/components/interruptions/InterruptionInfo.js
@@ -3,7 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 // React Bootstrap
-import { Container } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
+
+// Packages
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 // Components
 import Interruption from './Interruption';
@@ -26,6 +30,21 @@ const InterruptionInfo = () => {
 		<Container className='custom-container'>
 			<h2 className='text-center mb-3'>Interruption Information</h2>
 			<Interruption interruption={interruption_info} />
+
+			{interruption_info?.solution &&
+				<div>
+					<h4 className='text-center mb-3'>Solution</h4>
+					<Card className='text-success-emphasis bg-success-subtle text-center p-2'>
+						<Card.Body>
+							<ReactQuill
+								theme='bubble'
+								readOnly={true}
+								value={interruption_info?.solution && JSON.parse(interruption_info?.solution)}
+							/>
+						</Card.Body>
+					</Card>
+				</div>
+			}
 		</Container>
 	);
 }
